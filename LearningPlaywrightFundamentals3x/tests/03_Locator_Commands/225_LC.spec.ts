707acb0406aa522b8fd2cb9d10d30848950a3b61
KALTURA\ishank.walia <ishank.walia@kaltura.com>
2026-09-12 20:40:18 +0530

    Add Playwright fundamentals test examples

---
+++ LearningPlaywrightFundamentals3x/tests/03_Locator_Commands/225_LC.spec.ts
@@ -0,0 +1,8 @@
+import {test, expect} from '@playwright/test'
+
+test("Verify X", async({page})=>{
+
+    await page.goto(
+        "https://app.thetestingacademy.com/playwright/multiple_element_filter");
+
+});
