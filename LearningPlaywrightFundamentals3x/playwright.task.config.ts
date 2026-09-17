289fd8a341945df0c2eecd8116a16f123f0ff79b
Ishank Walia <ishank.walia398@gmail.com>
2026-09-17 23:38:46 +0530

    Delete LearningPlaywrightFundamentals3x/playwright.task.config.ts

--- LearningPlaywrightFundamentals3x/playwright.task.config.ts
+++
@@ -1,8 +0,0 @@
-﻿import { defineConfig } from '@playwright/test';
-export default defineConfig({
-  testDir: '.',
-  fullyParallel: false,
-  workers: 1,
-  reporter: 'line',
-  use: { headless: false }
-});
